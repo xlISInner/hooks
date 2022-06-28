9306a8ce1dddd44d54e04d2ecb5636b98604591e
xlISInner <60032012+xlISInner@users.noreply.github.com>
2022-06-28 13:18:23 +0300

    Update sahook.lua

--- Hooks/sahook.lua
+++ Hooks/sahook.lua
@@ -2,13 +2,11 @@ local oldnamecall
 oldnamecall = hookmetamethod(game, "__namecall", function(Self, ...)
     local Method = getnamecallmethod()
     local Args = {...}
-    if Method == "FindPartOnRayWithIgnoreList" and Toggles.silentTgl.Value == true and pos ~= nil then
-        print(pos)
-        Args[1] = PositionToRay(Camera.CFrame.Position, pos)
-        warn(pos)
+    if Method == "FindPartOnRayWithIgnoreList" and Toggles.silentTgl.Value == true and getgenv().pos ~= nil then
+        print(getgenv().pos)
+        Args[1] = PositionToRay(Camera.CFrame.Position, getgenv().pos)
+        warn(getgenv().pos)
         return oldnamecall(Self, unpack(Args))
     end
     return oldnamecall(Self, unpack(Args))
 end)
-
-RunService:BindToRenderStep("sa", 120, silentClosetPlayer)
