5cfa3b0c2cf2ce3a87bbe2ac6e2d623e9a8dda0d
xlISInner <60032012+xlISInner@users.noreply.github.com>
2022-06-28 11:24:50 +0300

    Create mm2_silenta.lua

---
+++ Hooks/mm2_silenta.lua
@@ -0,0 +1,20 @@
+local old
+old = hookmetamethod(game, "__namecall", function(self, ...)
+	local method = getnamecallmethod()
+	local args = { ... }
+	if not checkcaller() then
+		if typeof(self) == "Instance" then
+			if self.Name == "ShootGun" and method == "InvokeServer" then
+				if Toggles.sherifSilent.Value then
+					if murderer then
+						local base = murderer.Character.PrimaryPart
+						local velocity = base.AssemblyLinearVelocity
+						local aimpos = base.Position + (velocity * Vector3.new(Options.shefiSilentPrediction.Value / 200, 0, Options.shefiSilentPrediction.Value / 200))
+						args[2] = aimpos
+					end
+				end
+			end
+		end
+	end
+	return old(self, unpack(args))
+end)
