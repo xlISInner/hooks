f593e1939bc71288f22a5fcfd7c4172800777f47
xlISInner <60032012+xlISInner@users.noreply.github.com>
2022-06-28 13:28:41 +0300

    Update sahook.lua

--- Hooks/sahook.lua
+++ Hooks/sahook.lua
@@ -1,10 +1,12 @@
+local Camera = workspace.CurrentCamera
+
 local oldnamecall
 oldnamecall = hookmetamethod(game, "__namecall", function(Self, ...)
     local Method = getnamecallmethod()
     local Args = {...}
     if Method == "FindPartOnRayWithIgnoreList" and Toggles.silentTgl.Value == true and getgenv().pos ~= nil then
         print(getgenv().pos)
-        Args[1] = Ray.new(Camera.CFrame.Position, (getgenv().pos - Camera.CFrame.Position).Unit * 600)
+        Args[1] = PositionToRay(Camera.CFrame.Position, getgenv().pos)
         warn(getgenv().pos)
         return oldnamecall(Self, unpack(Args))
     end
