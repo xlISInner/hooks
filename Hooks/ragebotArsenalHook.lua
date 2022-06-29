07299fbcf000295ae77e44ba2440992869d197b4
xlISInner <60032012+xlISInner@users.noreply.github.com>
2022-06-29 22:45:23 +0300

    Update ragebotArsenalHook.lua

--- Hooks/ragebotArsenalHook.lua
+++ Hooks/ragebotArsenalHook.lua
@@ -4,9 +4,6 @@ local oldragehook
 oldragehook = hookmetamethod(game, "__namecall", function(Self, ...)
     local Method = getnamecallmethod()
     local Args = {...}
-    if Method == "FindPartOnRayWithIgnoreList" and Toggles.wallsBypassTgl.Value == true then
-        table.insert(Args[2], workspace.Map)
-    end
     if Method == "FindPartOnRayWithIgnoreList" and Toggles.ragebotTGL.Value == true and getgenv().RageTarget ~= nil then
         Args[1] = Ray.new(Camera.CFrame.Position, (getgenv().RageTarget - Camera.CFrame.Position).Unit * 600)
         return oldragehook(Self, unpack(Args))
