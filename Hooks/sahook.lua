c81861d4ccb743152b06abb492d92ba2b9d83609
xlISInner <60032012+xlISInner@users.noreply.github.com>
2022-07-07 09:41:18 +0300

    Update sahook.lua

--- Hooks/sahook.lua
+++ Hooks/sahook.lua
@@ -1,12 +1,16 @@
+local MarketplaceService = game:GetService("MarketplaceService")
 local Camera = workspace.CurrentCamera
 
-local oldnamecall
-oldnamecall = hookmetamethod(game, "__namecall", function(Self, ...)
+local oldragehook
+oldragehook = hookmetamethod(game, "__namecall", function(Self, ...)
     local Method = getnamecallmethod()
     local Args = {...}
-    if Method == "FindPartOnRayWithIgnoreList" and Toggles.silentTgl.Value == true and getgenv().pos ~= nil then
-        Args[1] = Ray.new(Camera.CFrame.Position, (getgenv().pos - Camera.CFrame.Position).Unit * 1000)
-        return oldnamecall(Self, unpack(Args))
+    if Method == "FindPartOnRayWithIgnoreList" and Toggles.ragebotTGL.Value == true and getgenv().RageTarget ~= nil then
+        Args[1] = Ray.new(Camera.CFrame.Position, (getgenv().RageTarget - Camera.CFrame.Position).Unit * 1000)
+        if Toggles.wallbangTgl.Value == true then
+            table.insert(Args[2], workspace.Map)
+        end
+        return oldragehook(Self, unpack(Args))
     end
-    return oldnamecall(Self, unpack(Args))
+    return oldragehook(Self, unpack(Args))
 end)
