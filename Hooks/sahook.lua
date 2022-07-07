6903512f8fd08ff2a7fe8fbe4bfbbb10e328fd81
xlISInner <60032012+xlISInner@users.noreply.github.com>
2022-07-07 09:50:19 +0300

    Update sahook.lua

--- Hooks/sahook.lua
+++ Hooks/sahook.lua
@@ -5,7 +5,7 @@ local oldragehook
 oldragehook = hookmetamethod(game, "__namecall", function(Self, ...)
     local Method = getnamecallmethod()
     local Args = {...}
-    if Method == "FindPartOnRayWithIgnoreList" and Toggles.ragebotTGL.Value == true and getgenv().RageTarget ~= nil then
+    if Method == "FindPartOnRayWithIgnoreList" and Toggles.ragebotTGL.Value == true and getgenv().pos ~= nil then
         Args[1] = Ray.new(Camera.CFrame.Position, (getgenv().RageTarget - Camera.CFrame.Position).Unit * 1000)
         return oldragehook(Self, unpack(Args))
     end
