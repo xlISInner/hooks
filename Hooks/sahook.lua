107ba0f41158d50eb2fbfcb06b605c6ddb8476e4
xlISInner <60032012+xlISInner@users.noreply.github.com>
2022-07-07 09:49:44 +0300

    Update sahook.lua

--- Hooks/sahook.lua
+++ Hooks/sahook.lua
@@ -7,9 +7,6 @@ oldragehook = hookmetamethod(game, "__namecall", function(Self, ...)
     local Args = {...}
     if Method == "FindPartOnRayWithIgnoreList" and Toggles.ragebotTGL.Value == true and getgenv().RageTarget ~= nil then
         Args[1] = Ray.new(Camera.CFrame.Position, (getgenv().RageTarget - Camera.CFrame.Position).Unit * 1000)
-        if Toggles.wallbangTgl.Value == true then
-            table.insert(Args[2], workspace.Map)
-        end
         return oldragehook(Self, unpack(Args))
     end
     return oldragehook(Self, unpack(Args))
