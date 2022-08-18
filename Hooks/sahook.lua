0329e96d94f7106edc6f4c25b6e9c8fc119f1b93
xlISInner <60032012+xlISInner@users.noreply.github.com>
2022-08-19 00:16:27 +0300

    Update sahook.lua

--- Hooks/sahook.lua
+++ Hooks/sahook.lua
@@ -5,9 +5,19 @@ local oldragehook
 oldragehook = hookmetamethod(game, "__namecall", function(Self, ...)
     local Method = getnamecallmethod()
     local Args = {...}
-    if Method == "FindPartOnRayWithIgnoreList" and Toggles.ragebotTGL.Value == true and getgenv().pos ~= nil then
-        Args[1] = Ray.new(Camera.CFrame.Position, (getgenv().RageTarget - Camera.CFrame.Position).Unit * 1000)
+    if Method == "FindPartOnRayWithIgnoreList" and Toggles.silentTgl.Value == true and getgenv().pos ~= nil then
+        Args[1] = Ray.new(Camera.CFrame.Position, (getgenv().pos - Camera.CFrame.Position).Unit * 1000)
+        if Toggles.wallbangTgl.Value == true then
+            table.insert(Args[2], workspace.Map)
+        end
         return oldragehook(Self, unpack(Args))
     end
+
+    if Self.Name == "UpdatePing" and Toggles.wallbangTgl.Value then
+        Args[1] = 2000
+        return oldragehook(Self, unpack(Args))
+    end
+
+    if Method == "FindPartOnRayWithIgnoreList" and Toggles.rage
     return oldragehook(Self, unpack(Args))
 end)
