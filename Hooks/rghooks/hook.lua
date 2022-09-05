88585ba3fa3c4539091e99f102453c5980c81451
xlISInner <60032012+xlISInner@users.noreply.github.com>
2022-09-05 12:49:08 +0300

    Update hook.lua

--- Hooks/rghooks/hook.lua
+++ Hooks/rghooks/hook.lua
@@ -22,9 +22,9 @@ oldindex = hookmetamethod(game, "__index", newcclosure(function(self, key)
         if not checkcaller() then
             if key == "CFrame" and Toggles.gmToggle.Value and LocalPlayer.Character and LocalPlayer.Character:FindFirstChild("HumanoidRootPart") and LocalPlayer.Character:FindFirstChild("Humanoid") and LocalPlayer.Character:FindFirstChild("Humanoid").Health > 0 then
                 if self == LocalPlayer.Character.HumanoidRootPart then
-                    return DesyncTypes[1] or CFrame.new()
+                    return getgenv().DesyncTypes[1] or CFrame.new()
                 elseif self == LocalPlayer.Character.Head then
-                    return DesyncTypes[1] and DesyncTypes[1] + Vector3.new(0, LocalPlayer.Character.HumanoidRootPart.Size / 2 + 0.5, 0) or CFrame.new()
+                    return getgenv().DesyncTypes[1] and getgenv().DesyncTypes[1] + Vector3.new(0, LocalPlayer.Character.HumanoidRootPart.Size / 2 + 0.5, 0) or CFrame.new()
                 end
             end
         end
