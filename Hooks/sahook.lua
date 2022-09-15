f0d112aa8ffa5f00cb707ad26c0c99c3fe5a5015
xlISInner <60032012+xlISInner@users.noreply.github.com>
2022-09-15 12:18:59 +0300

    Update sahook.lua

--- Hooks/sahook.lua
+++ Hooks/sahook.lua
@@ -1,5 +1,6 @@
 local MarketplaceService = game:GetService("MarketplaceService")
 local Camera = workspace.CurrentCamera
+local ExpectedArgs = {"LOL"}
 
 local oldragehook
 oldragehook = hookmetamethod(game, "__namecall", function(Self, ...)
@@ -22,5 +23,31 @@ oldragehook = hookmetamethod(game, "__namecall", function(Self, ...)
         return oldragehook(Self, unpack(Args))
     end
 
+    if Self.Name == 'UpdatePing' then
+        local number = table.getn(Args)
+        if number > 2 then
+            game.Players.LocalPlayer:Kick('Kicked by RGK to prevent an exploiting ban. Please report this in the buyers discord. Code: Invalid Args Count')
+        end
+        getgenv().Arg = Args[2]
+    end
+
     return oldragehook(Self, unpack(Args))
+end)
+
+-- Yielding the thread until the Arg[2] is found
+if not getgenv().Arg then
+    repeat
+        wait()
+    until getgenv().Arg
+end
+
+if not table.find(ExpectedArgs, tostring(getgenv().Arg)) then
+    game.Players.LocalPlayer:Kick('Kicked by RGK to prevent an exploiting ban. Please report this in the buyers discord. Code: Expected argument missing.')
+end
+
+spawn(function()
+    while wait(0.1) do
+        game.ReplicatedStorage.Events.UpdatePing:FireServer(500, 'LOL')
+        game.Players.LocalPlayer.Ping.Value = math.random(30, 70)
+    end
 end)
